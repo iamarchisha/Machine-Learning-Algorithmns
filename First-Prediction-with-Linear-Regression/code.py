27be2cc487cc71856de3c7e1bf8545e9f90549ae
Archisha Chandel <47753303+archisha-chandel@users.noreply.github.com>
2019-06-07 14:31:23 +0530

    Update code.py

--- First-Prediction-with-Linear-Regression/code.py
+++ First-Prediction-with-Linear-Regression/code.py
@@ -1,48 +1,15 @@
-# --------------
 import pandas as pd
 import numpy as np
 from sklearn.cross_validation import train_test_split
 # code starts here
+
+# read the dataset
 df = pd.read_csv(path)
-df.head(5)
-X = df.iloc[:,:-1]
-y = df.iloc[:,-1]
-X_train, X_test, y_train, y_test = train_test_split(X, y, 
-                                                    test_size=0.3, 
-                                                    random_state=6)
 
-# code ends here
-
-
-
-# --------------
-import matplotlib.pyplot as plt
-
-# code starts here        
+# print first five columns
+print(df.head())
 
 # store independent variable
-X = df.drop('list_price',axis=1)
-
-# store dependent variable
-y = df['list_price']
-X_train, X_test, y_train, y_test = train_test_split(X, y, 
-                                                    test_size=0.3, 
-                                                    random_state=6)
-cols = X_train.columns
-# ['ages','num_reviews','piece_count','play_star_rating','review_difficulty','star_rating','theme_name','val_star_rating','country']
-
-fig, axes = plt.subplots(nrows=3 , ncols=3)
-
-for i in range(0,3):
-    for j in range(0,3):
-        col = cols[i*3+j]
-        axes[i,j].scatter(X_train[col],y_train)
-# code ends here
-
-
-
-# --------------
-# Code starts here
 X = df.drop('list_price',axis=1)
 
 # store dependent variable
@@ -51,6 +18,13 @@ y = df['list_price']
 # spliting the dataset
 X_train,X_test,y_train,y_test=train_test_split(X,y ,test_size=0.3,random_state=6)
 # code ends here
+
+
+
+
+import matplotlib.pyplot as plt
+
+# code starts here        
 cols = X_train.columns
 
 fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(20,20))
@@ -68,36 +42,65 @@ for i in range(0,3):
 plt.show()
 
 
+
+
+# Code starts here
+
+# corr code
 corr = X_train.corr()
-X_train = X_train.drop('play_star_rating',axis=1)
-X_train = X_train.drop('val_star_rating',axis=1)
-X_test = X_test.drop('play_star_rating',axis=1)
-X_test = X_test.drop('val_star_rating',axis=1)
-X_train.head()
+print(corr)
+# drop columns from X_train
+X_train.drop(['play_star_rating','val_star_rating'],axis = 1 ,inplace=True)
+
+# drop columns from X_test
+X_test.drop(['play_star_rating','val_star_rating'], axis = 1 ,inplace=True)
+
 # Code ends here
 
 
-# --------------
+
+
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
 # Code starts here
-regressor = LinearRegression()
+
+#Instantiate linear regression model
+regressor=LinearRegression()
+
+# fit the model
 regressor.fit(X_train,y_train)
-y_pred = regressor.predict(X_test)
+
+# predict the result
+y_pred =regressor.predict(X_test)
+
+# Calculate mse
 mse = mean_squared_error(y_test, y_pred)
-print('mse : ',mse)
+
+# print mse
+print(mse)
+
+# Calculate r2_score
 r2 = r2_score(y_test, y_pred)
-print('r squared: ',r2)
+
+#print r2
+print(r2)
+
 # Code ends here
 
 
-# --------------
+
+
 # Code starts here
-residual = y_test - y_pred
-residual.hist()
+# calculate the residual
+residual = (y_test - y_pred)
 
+# plot the figure for residual
+plt.figure(figsize=(15,8))
+plt.hist(residual, bins=30)
+plt.xlabel("Residual")
+plt.ylabel("Frequency")   
+plt.title("Error Residual plot")
+plt.show()
 
 # Code ends here
-
-
